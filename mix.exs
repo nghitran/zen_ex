df96e543aee24e6491907ffbe2d4ee8a5d072e82
Nghi Tran <tpmnghi@gmail.com>
2018-09-04 15:43:11 +0700

    downgrade the version of Poison to be compatible with Phoenix framework

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule ZenEx.Mixfile do
       {:espec, "~> 1.5.1", only: :test},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:httpotion, "~> 3.1.0"},
-      {:poison, "~> 4.0"}
+      {:poison, "~> 3.0"}
     ]
   end
 end
